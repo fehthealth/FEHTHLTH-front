d2eb9817c8eeded6f380dae1a9f625d19c650889
Akanwealth <akan.asanga@gmail.com>
2025-01-29 19:01:30 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 
 import fehtHealthLogo from "../../public/assets/feht-health.png";
 import Link from "next/link";
+import PrimaryButton from "@/components/shared/buttons/PrimaryButton";
 
 const page = () => {
   return (
@@ -10,12 +12,34 @@ const page = () => {
       <div className="bg-secondary px-20 py-16 flex flex-col items-center gap-2 backdrop-blur-lg rounded-xl">
         <Image src={fehtHealthLogo} alt="feht logo" className="w-80" />
         <h1 className="mt-10 text-5xl font-grotesk font-bold">Coming Soon!!</h1>
-        <h3 className="mt-6 text-2xl">We are almost there</h3>
-        <p>Stay tuned for something amazing!</p>
+        {/* <h3 className="mt-6 text-2xl">We are almost there</h3> */}
+        <p className="mt-5 max-w-md">
+          Welcome to FEHTHealth, where we bridge the gap between patients and
+          doctors worldwide. Our platform empowers you to connect with top
+          healthcare professionals from the comfort of your home—anytime,
+          anywhere. Whether you need expert advice, a second opinion, or ongoing
+          care, we make it simple, secure, and accessible. Your health, your
+          way!
+        </p>
+        <div className="mt-10">
+          <PrimaryButton
+            text="Click to Join the WaitList"
+            color="text-primary"
+            borderColor="border-primary"
+            rounded="rounded-md"
+            onClick={() =>
+              window.open(
+                "https://docs.google.com/forms/d/e/1FAIpQLSeqOUGmzLkhuo5Ss5r5S7q0qxRDP0t4m3x5IX1GsKE7HrD4aw/viewform?usp=header",
+                "_blank"
+              )
+            }
+          />
+        </div>
         <div className="mt-6 flex flex-col items-center">
           <p className="">
             Have questions or need assistance? We&apos;re here to help!{" "}
           </p>
+
           <Link
             href="https://forms.gle/rmYjkoPXptBNJToA6"
             target="_blank"
