139c730263e4c964119854038216fd8185cfdab0
Akanwealth <akan.asanga@gmail.com>
2025-02-19 10:26:01 +0100

    removed comment code

--- src/redux/customStorage.ts
+++ src/redux/customStorage.ts
@@ -1,7 +1,6 @@
 import storage from "redux-persist/lib/storage"; // Keep this import
 import createWebStorage from "redux-persist/lib/storage/createWebStorage";
 
-// Create a no-op storage for environments where localStorage is not available
 const createNoopStorage = () => {
   return {
     getItem(_key: string) {
@@ -16,7 +15,6 @@ const createNoopStorage = () => {
   };
 };
 
-// Rename this to avoid conflict with the imported storage
 const customStorage =
   typeof window !== "undefined"
     ? createWebStorage("local")
